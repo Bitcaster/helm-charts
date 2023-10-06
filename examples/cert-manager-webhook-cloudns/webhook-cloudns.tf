41c1479a54cdbe24862de80c1ce29674ca9ea5f5
michael <michael@bitcaster.de>
2023-10-06 15:36:23 +0200

    version fixes

--- examples/cert-manager-webhook-cloudns/webhook-cloudns.tf
+++ examples/cert-manager-webhook-cloudns/webhook-cloudns.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cert-manager-webhook-cloudns" {
   name       = "webhook-cloudns"
   repository = "https://bitcaster.github.io/helm-charts/"
   chart      = "cert-manager-webhook-cloudns"
-  version    = "1.0.1"
+  version    = "1.0.3"
   namespace  = "cert-manager"
 
   values = [
